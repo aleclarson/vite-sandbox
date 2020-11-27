65d05bb400877c193d65b41674c010cddc0a69ff
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-11-27 13:13:33 -0500

    chore: build for debugging

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import reactPlugin from 'vite-plugin-react'
 
 const config: UserConfig = {
   plugins: [reactPlugin],
+  minify: false,
+  sourcemap: true,
+  env: {
+    NODE_ENV: 'development',
+  },
 }
 
 export default config
