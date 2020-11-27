c771221289a449d0adbc51649262f95158bddc87
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-11-27 12:30:34 -0500

    chore: add react

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { UserConfig } from 'vite'
+import reactPlugin from 'vite-plugin-react'
 
-const config: UserConfig = {}
+const config: UserConfig = {
+  plugins: [reactPlugin],
+}
 
 export default config
